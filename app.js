79fe1d290513be0df284032aeeca82ebcf85635b
Tehreem fatima syed amir <163660662+tehreemamir123@users.noreply.github.com>
2025-09-24 09:38:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let typed= new Typed(".txt", {
+    strings:["Full stack Mobile Application Developer" , "Certified Flutter App Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
